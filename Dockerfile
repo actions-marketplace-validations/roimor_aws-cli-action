ebf52d54cea490b77cd73f1ca7519cab5ecda4ad
roimor <roimor6@gmail.com>
2021-06-11 05:55:27 +0300

    small update

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM python:3.8.6-alpine3.11
 
 RUN pip3 --quiet --no-cache-dir install awscli
 
-ENTRYPOINT ["aws"]
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo `$*`
+sh -c "echo $*"
+
+aws configure set default.region "${REGION}"
+aws configure set aws_access_key_id "${AWS_SECRET_KEY_ID}"
+aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}"
+
+aws s3 cp "${SOURCE}" "${DESTINATION}"
